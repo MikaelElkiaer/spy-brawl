0958e76a29320ffe9484a5cecfafb0ebadf0d511
mikaelec <mikaelec@gmail.com>
2015-11-15 17:31:36 +0100

    Moved bower_components to public

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var io = require('socket.io')(http);
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/public', express.static('public'));
-app.use('/bower_components', express.static('bower_components'));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
