96a6aedbc7e915dc6ffa1cb6d5386bc4a3af8143
mikaelec <mikaelec@gmail.com>
2016-02-08 19:24:27 +0100

    Clean up in socket service, changed to non-min angular libs.

--- public/app/socket.factory.js
+++ public/app/socket.factory.js
@@ -9,25 +9,31 @@
 
   function factory($rootScope) {
     var socket = io.connect();
-    return {
-      on: function (eventName, callback) {
-        socket.on(eventName, function () {  
-          var args = arguments;
-          $rootScope.$apply(function () {
-            callback.apply(socket, args);
-          });
-        });
-      },
-      emit: function (eventName, data, callback) {
-        socket.emit(eventName, data, function () {
-          var args = arguments;
-          $rootScope.$apply(function () {
-            if (callback) {
-              callback.apply(socket, args);
-            }
-          });
-        })
-      }
+
+    var service = {
+      emit: emit,
+      on: on
     };
-  }  
+    return service;
+
+    function emit(eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+
+    function on(eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    }
+  }
 })();
