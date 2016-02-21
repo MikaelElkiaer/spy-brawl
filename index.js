ffe929fe70a8aec8108b412026f107b7dbff20c5
mikaelec <mikaelec@gmail.com>
2016-02-21 21:45:16 +0100

    Set jade as node view engine.

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
-
+app.set('view engine', 'jade');
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
+  res.render(`${__dirname}/public/index`);
 });
 
 var rooms = ['Room 1', 'Room 2', 'Room 3'];
