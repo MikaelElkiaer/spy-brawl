712908b33a0139e5816a59cba9e83de8e5cda994
mikaelec <mikaelec@gmail.com>
2016-02-22 15:24:18 +0100

    Added methods for getting/setting userSid and username.

--- public/app/user.service.js
+++ public/app/user.service.js
@@ -1,6 +1,34 @@
 class UserService {
+
   constructor(localStorageService) {
     this.storage = localStorageService;
+
+    this._userSidKey = 'userSid';
+    this._usernameKey = 'username';
+  }
+
+  clearUserSid() {
+    this.userSid = '';
+  }
+
+  clearUsername() {
+    this.username = '';
+  }
+
+  get userSid() {
+    return this.storage.get(this._userSidKey);
+  }
+
+  get username() {
+    return this.storage.get(this._userSidKey);
+  }
+
+  set userSid(sid) {
+    this.storage.set(this._usernameKey, sid);
+  }
+
+  set username(username) {
+    this.storage.set(this._usernameKey, username);
   }
 }
 
