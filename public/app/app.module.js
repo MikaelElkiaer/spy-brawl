187e348a082653325cb577bef3df03667989a10f
mikaelec <mikaelec@gmail.com>
2016-02-22 12:23:57 +0100

    Added angular-local-storage dependency with new config block.

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,3 +1,4 @@
+import { config } from './app.config.js';
 import { routeConfig } from './app.route.js';
 import { run } from './app.run.js';
 import { SocketService } from './socket.service.js';
@@ -6,11 +7,13 @@ import { RoomController } from './room.controller.js';
 
 var app = angular
   .module('app', [
-    'ui.router'
+    'ui.router',
+    'LocalStorageModule'
   ])
   .service('socketService', SocketService)
   .controller('homeController', HomeController)
   .controller('roomController', RoomController)
+  .config(config)
   .config(routeConfig)
   .run(run);
 
