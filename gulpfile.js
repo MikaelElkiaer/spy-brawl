54c9210b73ef6f9db40480e8dba054f04f8a7ee9
mikaelec <mikaelec@gmail.com>
2016-02-27 14:51:26 +0100

    Cleaned up gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,22 @@ var gulp = require('gulp'),
     spawn = require('child_process').spawn,
     node;
 
-gulp.task('server', function() {
-  if (node) node.kill();
+gulp.task('server', cb => {
+  if (node)
+    node.kill();
   node = spawn('node', ['index.js'], {stdio: 'inherit'});
   node.on('close', function (code) {
-    if (code === 8) {
+    if (code === 8)
       gulp.log('Error detected, waiting for changes...');
-    }
   });
+  cb();
 });
 
-gulp.task('default', ['server'], function() {
-  gulp.watch(['./index.js'], ['server']);
+gulp.task('default', ['server'], () => {
+  return gulp.watch(['./index.js'], ['server']);
 });
 
-process.on('exit', function() {
-    if (node) node.kill();
+process.on('exit', () => {
+    if (node)
+      node.kill();
 });
