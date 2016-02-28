7856400752888885a2e23c8e130ab491fe6c9204
mikaelec <mikaelec@gmail.com>
2016-02-28 08:57:14 +0100

    Added modal for changing username.

--- public/app/controllers/nav.controller.js
+++ public/app/controllers/nav.controller.js
@@ -1,12 +1,32 @@
 class NavController {
-  constructor(userService) {
+  constructor($uibModal, toastr, userService) {
+    this.$modal = $uibModal;
+    this.toastr = toastr;
     this.userService = userService;
 
     this._setup();
   }
 
   changeUsernameModal() {
-    
+    var theModal = this.$modal.open({
+      animation: true,
+      size: 'sm',
+      templateUrl: '/views/change-username-modal',
+      controller: ['$scope', '$uibModalInstance', 'username', function ($scope, theModal, username) {
+        $scope.newUsername = username;
+        $scope.save = () => { theModal.close($scope.newUsername); };
+        $scope.cancel = () => { theModal.dismiss('cancel'); };
+      }],
+      resolve: {
+        username: () => this.username
+      }
+    });
+
+    theModal.result.then(newUsername => {
+      this.username = newUsername;
+    }, reason => {
+        this.toastr.warning('Username was not changed...');
+    });
   }
 
   _setup () {
@@ -14,6 +34,6 @@ class NavController {
   }
 }
 
-NavController.$inject = ['userService'];
+NavController.$inject = ['$uibModal', 'toastr', 'userService'];
 
 export { NavController };
