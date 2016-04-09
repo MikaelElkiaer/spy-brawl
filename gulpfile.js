3b072b8d5ad2f532f3ab1b728392b8ef2186cf69
mikaelec <mikaelec@gmail.com>
2016-04-09 15:20:16 +0200

    Changed the way the server is restarted and combined start and watch into a single script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
-    spawn = require('child_process').spawn,
-    node;
+    server = require( 'gulp-develop-server' );
 
 gulp.task('watcher', cb => {
   livereload.listen();
@@ -12,22 +11,8 @@ gulp.task('watcher', cb => {
   cb();
 });
 
-gulp.task('server', cb => {
-  if (node)
-    node.kill();
-  node = spawn('node', ['index.js'], {stdio: 'inherit'});
-  node.on('close', function (code) {
-    if (code === 8)
-      gulp.log('Error detected, waiting for changes...');
-  });
-  cb();
-});
+gulp.task('server', () => {
+  server.listen( { path: './index.js' } );
 
-gulp.task('default', ['server'], () => {
-  return gulp.watch(['./index.js'], ['server']);
-});
-
-process.on('exit', () => {
-    if (node)
-      node.kill();
+  return gulp.watch( [ './index.js' ], server.restart );
 });
